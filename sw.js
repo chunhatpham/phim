9b6505f0c504ff8267ef7ad02a8897f77969783b
chunhatpham <jickjame84@gmail.com>
2026-03-13 01:11:22 +0700

    Delete sw.js

--- sw.js
+++
@@ -1,7 +0,0 @@
-self.addEventListener('install', (e) => {
-    console.log('[Service Worker] Đã cài đặt');
-});
-
-self.addEventListener('fetch', (e) => {
-    // Để trống để pass điều kiện PWA
-});
